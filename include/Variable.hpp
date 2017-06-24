6e5d47411634f08e23216f6321f5d18323fed1c6
S.V. Paulauskas <stanpaulauskas@gmail.com>
2017-06-24 17:31:39 -0400

    Updates documentation and formatting

--- include/Variable.hpp
+++ include/Variable.hpp
@@ -12,139 +12,183 @@
 
 #include <cmath>
 
+///Defines the Variable class. A physical quantity always has a value, error, and units. This class provides a way to
+/// organize that information, and propagates the errors as mathematical operations are performed on the variables.
+/// At the moment we only accept quantities that have symmetric error bars.
 class Variable {
 public:
+    /// Default constructor
     Variable(){};
 
+    /// Constructor that sets all the values.
+    /// @param[in] value : The value that the variable has
+    /// @param[in] error : The error on the units. We only accept symmetric error bars.
+    /// @param[in] units : The units on the variable.
     Variable(const double &value, const double &error,
-             const std::string &units) {
-        value_ = value;
-        error_ = error;
-        units_ = units;
-    };
+             const std::string &units) : value_(value), error_(error), units_(units) { };
+
+    /// Default Destructor
     ~Variable(){};
 
-    //Overloading the comparison operators here
-    inline bool operator== (const Variable &v) const {
-        if(UnitCheck(this->GetUnits(), v.GetUnits()))
-            return(this->GetValue() == v.GetValue());
-        else
-            return(false);
+    /// The Equality operator
+    /// @param[in] rhs : The variable that we are going to be comparing against
+    /// @returns True if the variables are equal
+    inline bool operator== (const Variable &rhs) const {
+        if(UnitCheck(this->GetUnits(), rhs.GetUnits()))
+            return this->GetValue() == rhs.GetValue();
+        return false;
     }; const
 
-    inline bool operator!= (const Variable &v) const { return(!(*this==v)); };
+    /// The Not Equality operator, just the negative of the equality operator
+    /// @param[in] rhs : The Variable that we are going to be comparing with.
+    /// @returns True if the variables are not equal
+    inline bool operator!= (const Variable &rhs) const { return !(*this==rhs); }
 
-    inline bool operator> (const Variable &v2) const {
-        if(UnitCheck(this->GetUnits(), v2.GetUnits()))
-            return(this->GetValue() > v2.GetValue());
-        else
-            return(false);
+    /// The Greater than operator
+    /// @param[in] rhs : The variable that we want to know if we're greater than.
+    /// @returns True if we were greater than the RHS
+    inline bool operator> (const Variable &rhs) const {
+        if(UnitCheck(this->GetUnits(), rhs.GetUnits()))
+            return this->GetValue() > rhs.GetValue();
+        return false;
     }; const
 
-    inline bool operator< (const Variable &v) const {
-        if(UnitCheck(this->GetUnits(), v.GetUnits()))
-            return(this->GetValue() < v.GetValue());
-        else
-            return(false);
+    /// The less than operator
+    /// @param[in] rhs : The variable that we want to check if we're less than
+    /// @returns True if we were less than the RHS
+    inline bool operator< (const Variable &rhs) const { return !(*this > rhs); }
+
+    /// The Greater than or equal operator
+    /// @param[in] rhs : The variable that we want to know if we're greater than or equal to
+    /// @returns True if we were greater than the RHS.
+    inline bool operator>= (const Variable &rhs) const {
+        if(UnitCheck(this->GetUnits(), rhs.GetUnits()))
+            return this->GetValue() >= rhs.GetValue();
+        return false;
     }; const
 
-    inline bool operator<= (const Variable &v) const {
-        if(UnitCheck(this->GetUnits(), v.GetUnits()))
-            return(this->GetValue() <= v.GetValue());
-        else
-            return(false);
-    }; const
+    /// The less than or equal to operator
+    /// @param[in] rhs : The variable that we want to check if we're less than
+    /// @returns True if we were less than the RHS
+    inline bool operator<= (const Variable &rhs) const { return !(*this >= rhs); }
 
-    inline bool operator>= (const Variable &v) const {
-        if(UnitCheck(this->GetUnits(), v.GetUnits()))
-            return(this->GetValue() >= v.GetValue());
-        else
-            return(false);
-    }; const
-
-    //Overloading the Unitary operators here
-    inline Variable operator+(const Variable&v) const {
-        if(!UnitCheck(this->GetUnits(), v.GetUnits()))
+    /// The addition operator. We can only add together variables that have matching units. Otherwise our
+    /// calculations are not physically meaningful
+    /// @param[in] rhs : The right hand side of our addition
+    /// @throws invalid_argument if we did not have matching units
+    /// @returns The sum of the two variables, along with the error propagated appropriately.
+    inline Variable operator+(const Variable&rhs) const {
+        if(!UnitCheck(this->GetUnits(), rhs.GetUnits()))
             return(Variable(0.0,0.0,""));
-        double val = this->GetValue() + v.GetValue();
-        double err = PmError(this->GetError(),v.GetError());
+        return Variable(this->GetValue() + rhs.GetValue(),
+                        CalculateAdditionSubtractionError(this->GetError(),rhs.GetError()), this->GetUnits());
+    }
+
+    /// The subtraction operator. We can only subtract variables that have matching units. Otherwise our
+    /// calculations are not physically meaningful
+    /// @param[in] rhs : The right hand side of our subtraction
+    /// @throws invalid_argument if we did not have matching units
+    /// @returns The sum of the two variables, along with the error propagated appropriately.
+    inline Variable operator-(const Variable&rhs) const {
+        if(!UnitCheck(this->GetUnits(), rhs.GetUnits()))
+            return(Variable(0.0,0.0,""));
+        double val = this->GetValue() - rhs.GetValue();
+        double err = CalculateAdditionSubtractionError(this->GetError(),rhs.GetError());
         std::string unit = this->GetUnits();
         return(Variable(val,err,unit));
-    };
+    }
 
-    inline Variable operator-(const Variable&v) const {
-        if(!UnitCheck(this->GetUnits(), v.GetUnits()))
-            return(Variable(0.0,0.0,""));
-        double val = this->GetValue() - v.GetValue();
-        double err = PmError(this->GetError(),v.GetError());
-        std::string unit = this->GetUnits();
-        return(Variable(val,err,unit));
-    };
-
-    inline Variable operator*(const Variable&v) const {
-        double val = this->GetValue() * v.GetValue();
-        double err = MdError(val,*this,v);
+    /// The multiplication operator. We currently concatenate the units and do not simplify them
+    /// @param[in] rhs : The right hand side of our product
+    /// @returns The product of the two variables, along with the error propagated appropriately.
+    inline Variable operator*(const Variable&rhs) const {
+        double val = this->GetValue() * rhs.GetValue();
+        double err = CalculateMultiplicationDivisonError(val,*this,rhs);
         std::stringstream ss;
-        ss << this->GetUnits() << "*" << v.GetUnits();
+        ss << this->GetUnits() << "*" << rhs.GetUnits();
         return(Variable(val,err,ss.str()));
-    };
+    }
 
+    /// The division operator. We currently concatenate the units and do not simplify them
+    /// @param[in] rhs : The right hand side of our division
+    /// @returns The quotient of the two variables, along with the error propagated appropriately.
     inline Variable operator/(const Variable&v) const {
         double val = this->GetValue() / v.GetValue();
-        double err = MdError(val,*this,v);
+        double err = CalculateMultiplicationDivisonError(val,*this,v);
         std::stringstream ss;
         ss << this->GetUnits() << "/" << v.GetUnits();
         std::string unit = this->GetUnits();
         return(Variable(val,err,ss.str()));
     };
 
+    /// The bitwise left shift operator, this operator is used so that we can output the variable to output streams
+    /// like cout.
+    /// @param[in] out : The output file stream that we want to use
+    /// @param[in] v : The variable that we want to output to the stream.
+    /// @returns A reference to the stream that we passed into the method.
     friend std::ostream& operator<<(std::ostream& out, const Variable &v) {
         out << std::to_string(v.GetValue()) << " +- " << std::to_string(v.GetError()) <<  " " << v.GetUnits() << " ";
         return out;
     }
 
-    inline void SetValue(const double &a) {value_ = a;};
-    inline void SetError(const double &a) {error_ = a;};
-    inline void SetUnits(const std::string &a) {units_ = a;};
+    /// Sets the value of the variable
+    /// @param[in] a : The value to set
+    inline void SetValue(const double &a) {value_ = a;}
 
-    inline double GetValue(void) const {return(value_);};
-    inline double GetError(void) const {return(error_);};
-    inline std::string GetUnits(void) const {return(units_);};
+    /// Sets the error on the variable
+    /// @param[in] a : The value to set
+    inline void SetError(const double &a) {error_ = a;}
 
-    inline std::string Output(void) const {
-        std::stringstream ss;
-        ss << value_ << " +- " << error_ << " " << units_;
-        return(ss.str());
-    };
+    /// Sets the units on the variable
+    /// @param[in] a : The value to set
+    inline void SetUnits(const std::string &a) {units_ = a;}
 
-    inline std::string OutputData(void) const {
-        std::stringstream ss;
-        ss << value_ << " " << error_;
-        return(ss.str());
-    };
+    /// Get the value of the Variable
+    /// @returns the value
+    inline double GetValue(void) const {return value_; }
+
+    /// Get the value of the error
+    /// @returns the error bars
+    inline double GetError(void) const {return error_;}
+
+    /// Get the value of the Variable
+    /// @returns the value
+    inline std::string GetUnits(void) const {return units_;}
+
 private:
-    double value_, error_;
-    std::string units_;
+    double value_; ///< The value of the Variable
+    double error_; ///< The error on the Variable
+    std::string units_; ///< The units on the Variable
 
-    bool UnitCheck(const std::string &u1, const std::string &u2) const {
-        if(u1 == u2)
-            return(true);
-        else {
-            // std::cerr << "WARNING!!!!! You are trying to operate on " 
-            //           << "two variables that have different units!!!" 
-            //           << std::endl << u1 << " != " << u2 << std::endl;
-            return(false);
-        }
+    /// Provides a check on the units for situations where we are adding or subtracting
+    ///@param[in] u1 : The units on the LHS
+    ///@param[in] u2 : The units on the RHS
+    bool UnitCheck(const std::string &lhs, const std::string &rhs) const {
+        if(lhs == rhs)
+            return true;
+        // std::cerr << "WARNING!!!!! You are trying to operate on "
+        //           << "two variables that have different units!!!"
+        //           << std::endl << u1 << " != " << u2 << std::endl;
+        return false;
     };
 
-    double PmError(const double &val1, const double &val2) const {
-        return(sqrt(val1*val1+val2*val2));
+    /// Propagates the error on addition and subtraction operations.
+    /// @param[in] lhs : The error bar on the left hand side
+    /// @param[in] rhs : The error bar on the right hand side
+    /// @returns the propagated error bar on the variable
+    double CalculateAdditionSubtractionError(const double &lhs, const double &rhs) const {
+        return sqrt(lhs*lhs + rhs*rhs);
     };
-    double MdError(const double &val, const Variable &v1,
-                   const Variable &v2) const {
-        return(val*
-               sqrt(v1.GetError()*v1.GetError()/v1.GetValue()/v1.GetValue() +
-                    v2.GetError()*v2.GetError()/v2.GetValue()/v2.GetValue()));
+
+    /// Propagates the error on addition and subtraction operations.
+    /// @param[in] val : The value of the original multiplication or division operation. This needs multiplied in to
+    /// properly propagate the error.
+    /// @param[in] lhs : The error bar on the left hand side
+    /// @param[in] rhs : The error bar on the right hand side
+    /// @returns the propagated error bar on the variable
+    double CalculateMultiplicationDivisonError(const double &val, const Variable &lhs, const Variable &rhs) const {
+        return val* sqrt(v1.GetError()*v1.GetError()/v1.GetValue()/v1.GetValue()
+                         + v2.GetError()*v2.GetError()/v2.GetValue()/v2.GetValue());
     }
 }; //class Variable
 #endif //__VARIABLE_HPP__
--- tests/unittest-Variable.cpp
+++ tests/unittest-Variable.cpp
@@ -16,45 +16,45 @@ int main () {
     Variable var2(123.45, 0.6789, "MeV");
     Variable var3(123.45, 3465.84, "keV/MeV");
     
-    cout << "The Current information for var: " << var.Output() << endl;
+    cout << "The Current information for var: " << var << endl;
 
     var.SetValue(987.65);
     var.SetUnits("hbar/c/G");
 
-    cout << "The Updated information for var: " << var.Output() << endl;
+    cout << "The Updated information for var: " << var << endl;
 
     cout << endl << "We are now going to try to compare things. " << endl;
     bool temp =  var1 == var2;
-    cout << "Does var1 = " << var1.Output() << " compare equal to var2 = " 
-         << var2.Output() << "?!?!" << endl << "Answer: " << temp << endl; 
+    cout << "Does var1 = " << var1 << " compare equal to var2 = " 
+         << var2 << "?!?!" << endl << "Answer: " << temp << endl; 
 
     bool temp1 = var1 == var3;
     cout << endl
-         << "Does var1 = " << var1.Output() << " compare equal to var3 = " 
-         << var3.Output() << "?!?!" << endl << "Answer: " << temp1 << endl;
+         << "Does var1 = " << var1 << " compare equal to var3 = " 
+         << var3 << "?!?!" << endl << "Answer: " << temp1 << endl;
 
     cout << "We are going to do some maths now." << endl;
     Variable add = var1 + var2;
-    cout << add.Output() << endl;
+    cout << add << endl;
     
     Variable add1 = var1 + var3;
-    cout << add1.Output() << endl;
+    cout << add1 << endl;
     
     Variable sub = var1 - var2;
-    cout << sub.Output() << endl;
+    cout << sub << endl;
     
     Variable sub1 = var1 - var3;
-    cout << sub1.Output() << endl;
+    cout << sub1 << endl;
 
     Variable mult = var1 * var2;
-    cout << mult.Output() << endl;
+    cout << mult << endl;
     
     Variable mult1 = var1 * var3;
-    cout << mult1.Output() << endl;
+    cout << mult1 << endl;
 
     Variable div = var1 / var2;
-    cout << div.Output() << endl;
+    cout << div << endl;
 
     Variable div1 = var1 / var3;
-    cout << div1.Output() << endl;
+    cout << div1 << endl;
 }
